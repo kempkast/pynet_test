e11811d5cd5a3965f71f01c9583f7ad5754f483c
kempkast <stacy.kempka@verizonwireless.com>
2016-11-28 14:32:35 -0800

    package

---
+++ reuseEX1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def func1():
+    print "func1"
+
+def func2():
+    print "func2"
+
+def func3():
+    print "func3"
+
+def main():
+    print "Hello world"
+
+if __name__ == "__main__":
+    main()
+
