08fc20bf25da2037895792df52f4c319c0ab3920
kempkast <stacy.kempka@verizonwireless.com>
2016-11-28 09:29:19 -0800

    String

---
+++ strEX1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+name1 = "Stacy"
+name2 = "Samantha"
+name3 = "Sarah"
+name4 = raw_input("Enter fourth name: ")
+
+print
+print "{:>30}".format(name1)
+print "{:>30}".format(name2)
+print "{:>30}".format(name3)
+print "{:>30}".format(name4)
+print
+
