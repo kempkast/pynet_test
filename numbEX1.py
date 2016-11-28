981557a240b7aff2e400b7768ce758bfe6cfe532
kempkast <stacy.kempka@verizonwireless.com>
2016-11-28 10:00:03 -0800

    number

---
+++ numbEX1.py
@@ -0,0 +1,11 @@
+#!/usr/bing/env python
+
+num1 = int(raw_input("Enter first number:"))
+num2 = int(raw_input("Enter second number:"))
+
+print "\nSum: {}".format(num1 + num2)
+print "Difference: {}".format(num1 - num2)
+print "Product: {}".format(num1 * num2)
+print "Division: {:.2f}".format(num1/float(num2))
+
+print
