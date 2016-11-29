8ea163a9bb27d4d5679d3a9abc7d5b599373ca97
kempkast <stacy.kempka@verizonwireless.com>
2016-11-29 09:35:25 -0800

    showversion

---
+++ nstest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+x = 20
+y = 30
+z = 40
+
+def my_func(x, y):
+    x = 72
+    print x
+    print y
+    print z
+
+my_func(x,y)
+print x
---
+++ snmp_simple_ex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import getpass
+import snmp_helper
+
+SYS_DESCR = '1.2.6.1.2.1.1.1.0'
+ip_addr1 = raw_imput("pynet-rtr1 IP address: ")
+community_string = getpass.getpass(prompt="Community strings: ")
+pynet_rtr1 = (ip_addr1, community_string, 161)
+
+snmp_data = snmp_helper.snmp_get_oid(pynet_rtr1, oid+SYS_DESCR)
+output = snmp_helper.snmp_extract(snmp_data)
+print output
+
